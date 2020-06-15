f55092bd8b24727125425dcbbce2965a50a20992
Joe V <joeveverka89@gmail.com>
2020-06-15 17:44:07 -0400

    added network data loader to marsrover client and refactored fetch func

---
+++ Astronomy/Helpers/URLSession+DataLoader.swift
@@ -0,0 +1,28 @@
+//
+//  URLSession+DataLoader.swift
+//  Astronomy
+//
+//  Created by Marissa Gonzales on 6/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession: NetworkDataLoader {
+
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: request) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+
+
+    }
+
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let task = self.dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }
+        task.resume()
+    }
+}
